59de12b4783edca968f281ef882f493f77cde264
Gercsi <Gercsi@DESKTOP-H06B5K2>
2019-12-09 19:28:29 +0100

    asdfg

---
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author Gercsi
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("asd");
+        // TODO code application logic here
+    }
+    
+}
